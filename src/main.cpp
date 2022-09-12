baf165c25ec1199d915859d4bbf210ee53314231
joshuasymons-webb <000812836@mohawkcollege.ca>
2022-09-12 16:34:23 -0400

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Name: Joshua Symons-Webb");
+  Serial.println("Student ID: 000812836");
+  Serial.println("Hello, World!\n");
+
+  Serial.print("\nESP8266 Chip ID: ");
+  Serial.print(ESP.getChipId());
+
+  Serial.print("\nFlash Chip ID: ");
+  Serial.print(ESP.getFlashChipId());
+}
+
+void loop() {
+  delay(2000);
+
+  Serial.print("\nThe D1 Mini has been running for ");
+  Serial.print(millis());
+  Serial.print(" seconds");
+}
